338c156f5f47e88662f9a4b1ac843e2cdfcb82a7
sergio Prieto <saprieto5@gmail.com>
2019-12-09 17:02:12 +0100

    Add principal function with hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleChange = (e) => {
+    const input = e.target;
+    let text = input.value;
+    
+    //creation d'une expression reguliere
+    var regex = RegExp('^[0-9]*$');
+    if (!regex.test(text)) {
+      console.log("character non valide !");
+      return;
+    }
+    setCount(Number(input.value));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Counter value is: {count}</h2>
+
+      <input type="text" value={count} onChange={handleChange}/>
+
+      <br />
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
 }
